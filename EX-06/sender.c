d0dc7189114e7a6c3ccfeb82e43f45dc661f4c7a
NandhanaCH <220701181@rajalakshmi.edu.in>
2024-06-01 13:40:38 +0530

    Update sender.c

--- EX-06/sender.c
+++ EX-06/sender.c
@@ -1,38 +1,31 @@
-//sender.c
-#include &lt;sys/types.h&gt;
-#include &lt;sys/ipc.h&gt;
-#include &lt;sys/shm.h&gt;
-#include &lt;stdio.h&gt;
-#include &lt;stdlib.h&gt;
-#include &lt;unistd.h&gt;
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #define SharedMemSize 50
 void main()
 {
-char c;
-int shmid;
-key_t key;
+        char c;
+        int shmid;
+        key_t key;
+        char *shared_memory;
+        key = 5677;
+        //Create segment with the key specified
+        if ((shmid = shmget(key, SharedMemSize, IPC_CREAT | 0666)) < 0)
+        {
 
-30
+        perror("shmget");
+        exit(1);
+        }
 
-char *shared_memory;
-
-key = 5677;
-//Create segment with the key specified
-if ((shmid = shmget(key, SharedMemSize, IPC_CREAT |
-0666)) &lt; 0)
-{
-//perror explains error code
-perror(&quot;shmget&quot;);
-exit(1);
-}
-//Attach the segment
-
-if((shared_memory= shmat(shmid, NULL, 0)) == (char *) -1)
-{
-perror(&quot;shmat&quot;);
-exit(1);
-}
-sprintf(shared_memory,&quot; Welcome to Shared Memory&quot;);
-sleep(2);
-exit(0);
+        if((shared_memory= shmat(shmid, NULL, 0)) == (char *) -1)
+        {
+        perror("shmat");
+        exit(1);
+        }
+            sprintf(shared_memory," Welcome to Shared Memory");
+        sleep(2);
+        exit(0);
 }
